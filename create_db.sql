a8b1a751e263a6db8d5161b372d631d8c2245c8f
Brendon Henrique <brendonhps@gmail.com>
2020-04-30 00:21:00 -0300

    fix database struct creation by script

--- create_db.sql
+++ create_db.sql
@@ -1,29 +1,30 @@
-CREATE DATABASE squadmanager;
-
-CREATE TABLE project (
-    project_id PRIMARY KEY NOT NULL,
-    date_start date NOT NULL,
-    date_finish date NOT NULL,
-    date_homologation_start date NOT NULL,
-    date_homologation_end date NOT NULL,
-    devs TEXT [],
-    FOREIGN KEY (deploy_id) REFERENCES deploy (deploy_id)
-    FOREIGN KEY (requirements_id) REFERENCES requirements (requirements_id)
-);
-
-CREATE TABLE deploy (
-    deploy_id PRIMARY KEY NOT NULL,
-    deploy_date date NOT NULL,
-    FOREIGN KEY (requirements_id) REFERENCES requirements (requirements_id)
+CREATE TABLE devs (
+                      dev_id INTEGER PRIMARY KEY NOT NULL,
+                      name VARCHAR(100) NOT NULL,
+                      age INTEGER NOT NULL
 );
 
 CREATE TABLE requirement (
-    requirement_id PRIMARY KEY NOT NULL,
-    dependencies TEXT []
+                             requirement_id INTEGER PRIMARY KEY NOT NULL,
+                             dependencies TEXT []
 );
 
-CREATE TABLE devs (
-    dev_id PRIMARY KEY NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    age INTEGER NOT NULL
+CREATE TABLE deploy (
+                        deploy_id INTEGER NOT NULL,
+                        deploy_date date NOT NULL,
+                        requirements_id INTEGER,
+                        PRIMARY KEY (deploy_id,requirements_id)
+);
+
+
+CREATE TABLE project (
+                         project_id INTEGER NOT NULL,
+                         date_start date NOT NULL,
+                         date_finish date NOT NULL,
+                         date_homologation_start date NOT NULL,
+                         date_homologation_end date NOT NULL,
+                         devs TEXT [],
+                         deploy_id INTEGER,
+                         requirements_id INTEGER,
+                         PRIMARY KEY (project_id,deploy_id,requirements_id)
 );
