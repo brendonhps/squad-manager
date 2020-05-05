490aafb249cea3444e1fac2749fee85e5193edc4
Brendon Henrique <brendonhps@gmail.com>
2020-05-05 00:02:39 -0300

    Create the handler of deploy create

--- Controllers/deployController.go
+++ Controllers/deployController.go
@@ -1,9 +1,30 @@
 package Controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"squad-manager/Models"
+)
 
 func CreateDeploy(w http.ResponseWriter, r *http.Request) {
+	deploy := Models.Deploy{}
+	err := json.NewDecoder(r.Body).Decode(&deploy)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	// Todo validate requirement id
+
+	err = Models.InsertDeploy(deploy)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.WriteHeader(200)
+	err = json.NewEncoder(w).Encode(deploy)
+	if err != nil {
+		panic(error(err))
+	}
 }
 
 func IndexDeploys(w http.ResponseWriter, r *http.Request) {
